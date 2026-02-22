464605290aaea64d717ee69da873a88b63e0776c
Ignacio Araya <ign.arayaa@duocuc.cl>
2026-02-22 20:08:08 -0300

    Create Cliente.java

---
+++ src/main/java/cl/speedfast/model/Cliente.java
@@ -0,0 +1,71 @@
+package cl.speedfast.model;
+
+public class Cliente {
+
+    private int id;
+    private String nombre;
+    private String telefono;
+    private String direccion;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombre, String telefono, String direccion) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public Cliente(int id, String nombre, String telefono, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    // Getters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    // Setters
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", direccion='" + direccion + '\'' +
+                '}';
+    }
+}
